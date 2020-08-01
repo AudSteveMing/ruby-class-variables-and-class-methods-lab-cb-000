42ef6d84d1d321fbcd15bdabcb7b72c3c2d69be3
Steve Cordova <cordovas@bc.edu>
2020-08-01 00:34:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+
+ @@count = 0
+ @@artists = []
+ @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist unless @@artists.include?(@artist)
+    @@genres << @genre unless @@genres.include?(@genre)
+
+  end
+
+  def new
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def self.genre_count
+  end
+
+  def self.artist_count
+  end
+
+
+  # def name
+  # end
+
+  # def artist=(artist)
+  # end
+  #
+  # def genre
+  # end
+end
